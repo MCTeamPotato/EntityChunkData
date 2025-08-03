e11eb469378a747febfd84da2085f1dec747fe39
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-08-03 14:11:26 +0800

    fix

--- src/main/java/me/kall/entitychunkdata/mixin/MinecraftServerMixin.java
+++ src/main/java/me/kall/entitychunkdata/mixin/MinecraftServerMixin.java
@@ -1,7 +1,7 @@
 package me.kall.entitychunkdata.mixin;
 
 import me.kall.entitychunkdata.data.EntitiesInChunkData;
-import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,9 +9,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.BooleanSupplier;
 
-@Mixin(ServerLevel.class)
-public abstract class ServerLevelMixin {
-    @Inject(method = "tick", at = @At("TAIL"))
+@Mixin(MinecraftServer.class)
+public abstract class MinecraftServerMixin {
+    @Inject(method = "tickServer", at = @At("TAIL"))
     private void onTickEnd(BooleanSupplier hasTimeLeft, CallbackInfo ci) {
         Runnable task;
         while ((task = EntitiesInChunkData.TASKS.poll()) != null) {
